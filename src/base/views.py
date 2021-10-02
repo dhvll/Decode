7a43b77a62782b993e0a8ee90f36d9d839d3a5d7
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-10-02 20:46:12 +0530

    2-Urls,Views,Templates
    Setup Basic Urls,VIews and Templates which are essentials for project building. In views, just use temporary data for basic setup.

--- src/base/views.py
+++ src/base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Lets learn django!'},
+    {'id': 2, 'name': 'Lets learn CSS!'},
+    {'id': 3, 'name': 'Lets learn Python!'},
+]
+
+def home(request):
+    context = {
+        'rooms': rooms,
+    }
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
