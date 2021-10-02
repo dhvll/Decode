db840652615ca0a3f2442de65eebab484813f59a
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-10-02 23:52:02 +0530

    3-Database and Admin Panel
    Added New Models - Message, Topic and customize Room Model. Models are added in admin panel and created few queries.

--- src/base/views.py
+++ src/base/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Lets learn django!'},
-    {'id': 2, 'name': 'Lets learn CSS!'},
-    {'id': 3, 'name': 'Lets learn Python!'},
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     context = {
         'rooms': rooms,
     }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
