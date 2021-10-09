6756630ac7de03901afd84bdade1fdceb665457b
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-10-09 20:10:01 +0530

    Activity Feed
    Activity Feed is done. So now user can see their live activity in home page and have access to their own activity.Divide home page into few parts and shift code to their own file so which will look clean and easy to access.

--- src/base/models.py
+++ src/base/models.py
@@ -7,12 +7,15 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
 class Room(models.Model):
     host = models.ForeignKey(User,  on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name="participants", blank=True)
+    participants = models.ManyToManyField(
+        User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -22,6 +25,7 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -29,8 +33,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.body[0:50]
-
-
-
