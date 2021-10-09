c953974890bb37b7fbe456e7402848bfe2a58e58
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-10-09 22:39:36 +0530

    Static File Config

--- src/decode/settings.py
+++ src/decode/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = os.path.join()
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
