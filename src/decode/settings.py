f239a45eea62030f31bc78ca9f80058553c7e9fb
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-10-14 14:16:35 +0530

    Error in runtime.txt and Updated whitenoise middleware

--- src/decode/settings.py
+++ src/decode/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
