5df172e16dccae97d19a8724204d10dc3d19b9a2
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-02-17 15:45:32 +0530

    deploy: add whitenoise to wsgi file

--- src/decode/wsgi.py
+++ src/decode/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decode.settings')
 
 application = get_wsgi_application()
+
+
+application = WhiteNoise(application)
